12d9f85619c56084fa5eb1651c980a921166f71d
Todor Trundev <ttrundev@visteon.com>
2020-08-21 12:15:33 +0300

    Allow workflow to be manually triggered
    Add workflow_dispatch event. Fix github-api.bat to trigger both repository
    and workflow events.

--- github-api.bat
+++ github-api.bat
@@ -2,14 +2,27 @@ set REPO=%1
 if _%REPO% == _ set REPO=github-package
 
 set WORKFLOW=main.yml
+set WORKFLOW_REF=master
+set WORKFLOW_INPUTS={\"IMAGE_NAME\": \"test-package\"}
 
-:: List repository tags (HTTP GET)
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/trundev/%REPO%/tags
+set OPT=
+set OPT=-H "Accept: application/vnd.github.v3+json" %OPT%
+::set OPT=--user trundev:<set GITHUB_TOKEN here> %OPT%
+set OPT=--user trundev %OPT%
 
-:: Create a repository dispatch event (HTTP POST)
-:: https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event
-curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/trundev/%REPO%/dispatches -d "{\"event_type\": \"event_type\", \"client_payload\": \"Payload\"}"
+@echo ===============================
+@echo List repository tags (HTTP GET)
+curl %OPT% https://api.github.com/repos/trundev/%REPO%/tags
+@echo.
 
-:: Create a workflow dispatch event (HTTP POST)
-:: https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
-curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/trundev/%REPO%/actions/workflows/%WORKFLOW%/dispatches -d "{\"ref\": \"ref\", \"inputs\": \"Inputs\"}"
+::See https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event
+@echo ==============================================
+@echo Create a repository dispatch event (HTTP POST)
+curl -X POST %OPT% https://api.github.com/repos/trundev/%REPO%/dispatches -d "{\"event_type\": \"my_event_type\", \"client_payload\": {}}"
+@echo.
+
+::See https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
+@echo ============================================
+@echo Create a workflow dispatch event (HTTP POST)
+curl -X POST %OPT% https://api.github.com/repos/trundev/%REPO%/actions/workflows/%WORKFLOW%/dispatches -d "{\"ref\": \"%WORKFLOW_REF%\", \"inputs\": %WORKFLOW_INPUTS%}"
+@echo.
