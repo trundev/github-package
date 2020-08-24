537c82b7723470ae21d7ec871e60cde24e7b59fa
Todor Trundev <trundev@gmail.com>
2020-08-24 22:32:35 +0300

    Delete package workflow experiment

--- github-api.bat
+++ github-api.bat
@@ -2,8 +2,8 @@ set REPO=%1
 if _%REPO% == _ set REPO=github-package
 
 set WORKFLOW=main.yml
-set WORKFLOW_REF=master
-set WORKFLOW_INPUTS={\"IMAGE_NAME\": \"test-package\"}
+set WORKFLOW_REF=delete-package
+set WORKFLOW_INPUTS={\"NAME\": \"test-github-package\"}
 
 set OPT=
 set OPT=-H "Accept: application/vnd.github.v3+json" %OPT%
@@ -13,12 +13,6 @@ set OPT=--user trundev %OPT%
 @echo ===============================
 @echo List repository tags (HTTP GET)
 curl %OPT% https://api.github.com/repos/trundev/%REPO%/tags
-@echo.
-
-::See https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event
-@echo ==============================================
-@echo Create a repository dispatch event (HTTP POST)
-curl -X POST %OPT% https://api.github.com/repos/trundev/%REPO%/dispatches -d "{\"event_type\": \"my_event_type\", \"client_payload\": {}}"
 @echo.
 
 ::See https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
